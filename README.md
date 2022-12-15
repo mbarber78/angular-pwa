b77750427eaf6110cecc42b143b8dfa8d65632aa
mbarber78 <matthew@mbit.ca>
2022-12-15 01:54:44 -0600

    1 binaries uploaded

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # angular-pwa
 
-This is the updated mobile app component to the Operation Red Nose solution for Manitoba driveteams. 
+This is the updated mobile app component to the Operation Red Nose solution for Manitoba driveteams.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+    ]),
+  ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
